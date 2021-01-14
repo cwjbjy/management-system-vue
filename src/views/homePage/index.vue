13f3fb423b94ba13248f5f0157ca77842e68001f
wj.cao <595842570@qq.com>
2021-01-14 18:22:33 +0800

    修改系统名称

--- src/views/homePage/index.vue
+++ src/views/homePage/index.vue
@@ -26,7 +26,7 @@
           <div slot="header" class="clearfix">
             <span>语言详情</span>
           </div>Vue
-          <el-progress :percentage="98" color="#42b983" :format="format"></el-progress>uni-app
+          <el-progress :percentage="98" color="#42b983" :format="format"></el-progress>Uni-app
           <el-progress :percentage="89" color="#f1e05a" :format="format"></el-progress>React
           <el-progress :percentage="88" :format="format"></el-progress>Nuxt
           <el-progress :percentage="87" color="#f56c6c" :format="format"></el-progress>
