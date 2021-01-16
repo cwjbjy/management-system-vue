64fbe4eb002aebc426a3bf4a584deb015194f887
wj.cao <595842570@qq.com>
2021-01-16 17:26:04 +0800

    删除注释

--- src/components/BaseEchartsModel/LineModel.vue
+++ src/components/BaseEchartsModel/LineModel.vue
@@ -94,41 +94,6 @@ export default {
             type: "line",
           },
         },
-        // dataZoom: [
-        //   {
-        //     type: "slider",
-        //     xAxisIndex: 0,
-        //     start: 0,
-        //     end: 100,
-        //     height: 20,
-        //     filterMode: "empty",
-        //     background: "rgba(1, 143, 252, 0.3)",
-        //     fillerColor: "rgba(43, 187, 239, 0.2)",
-        //     borderColor: "rgba(1, 143, 252, 0.6)",
-        //     handleStyle: {
-        //       color: blue,
-        //       borderColor: blue
-        //     },
-        //     handleIcon:
-        //       "M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z",
-        //     handleSize: "20",
-        //     textStyle: {
-        //       color: blue
-        //     },
-        //     dataBackground: {
-        //       areaStyle: {
-        //         color: "rgba(1, 143, 252, 0.1)"
-        //       }
-        //     }
-        //   },
-        //   {
-        //     type: "inside",
-        //     xAxisIndex: 0,
-        //     start: 30,
-        //     end: 60,
-        //     filterMode: "empty"
-        //   }
-        // ],
         yAxis: {
           type: "value",
           name: "次数",
