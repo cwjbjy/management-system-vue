69947bd2b66b9a1aef02c1d43806d63f0beb8dba
wj.cao <595842570@qq.com>
2022-02-13 15:32:03 +0800

    feat: 增加进度条 (story#000)

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,20 @@
 Vue.use(VueRouter);
 
-import { routes } from './routes'
+import { routes } from './routes';
+import NProgress from 'nprogress'; // progress bar
+import 'nprogress/nprogress.css'; // progress bar style
 
-const originalPush = VueRouter.prototype.push
+NProgress.configure({ showSpinner: false });
+
+const originalPush = VueRouter.prototype.push;
 VueRouter.prototype.push = function push(location) {
-    return originalPush.call(this, location).catch(err => err)
-}
+  return originalPush.call(this, location).catch((err) => err);
+};
 
 const router = new VueRouter({
-    mode: 'history',
-    routes
-})
+  mode: 'history',
+  routes,
+});
 
 /**
  * 路由全局前置守卫
@@ -18,19 +22,24 @@ const router = new VueRouter({
  * 没有登录则跳转到登录页面
  */
 router.beforeEach((to, from, next) => {
-    if (to.path === '/login') {
-        Vue.$cookies.keys().forEach(item => Vue.$cookies.remove(item))
-        next();
-    }
-    let token = Vue.$cookies.get('token');
+  NProgress.start();
+  if (to.path === '/login') {
+    Vue.$cookies.keys().forEach((item) => Vue.$cookies.remove(item));
+    next();
+  }
+  let token = Vue.$cookies.get('token');
 
-    if (!token && to.path !== '/login') {
-        next({
-            path: '/login'
-        })
-    } else {
-        next();
-    }
+  if (!token && to.path !== '/login') {
+    next({
+      path: '/login',
+    });
+  } else {
+    next();
+  }
 });
 
-export default router
+router.afterEach(() => {
+  NProgress.done();
+});
+
+export default router;
